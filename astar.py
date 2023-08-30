086ca836f992d2f3b7c131cd696ea5fc36443437
amitk <amitkarp@campus.technion.ac.il>
2023-08-30 16:22:22 +0300

    Rev 2.1

--- astar.py
+++ astar.py
@@ -1,5 +1,5 @@
 import pygame
-import math
+import numpy as np
 from queue import PriorityQueue
 
 WIDTH = 800
@@ -16,6 +16,87 @@ PURPLE = (128, 0, 128)
 ORANGE = (255, 165 ,0)
 GREY = (128, 128, 128)
 TURQUOISE = (64, 224, 208)
+# Colors for 6 agents
+# 1 start,2 outer circle, 3 agent, 4 inner circle, 5 end
+# 1
+#KHAKI = ((240, 230, 140), (255, 246, 143), (238, 230, 133), (205, 198, 115), (139, 134, 78))
+# 2
+#PALEGREEN = ((152, 251, 152), (154, 255, 154), (144, 238, 144), (124, 205, 124), (84, 139, 84))
+# 3
+#LIGHTPINK = ((255, 182, 193), (255, 174, 185), (238, 162, 173), (205, 140, 149), (139, 95, 101))
+# 4
+#ORCHID = ((218, 112, 214), (255, 131, 250), (238, 122, 233), (205, 105, 201), (139, 71, 137))
+# 5
+#LIGHTSKYBLUE = ((135, 206, 250), (176, 226, 255), (164, 211, 238), (141, 182, 205), (96, 123, 139))
+# 6
+#CORAL = ((255, 127, 80), (255, 114, 86), (238, 106, 80), (205, 91, 69), (139, 62, 47))
+# 7
+#DARKGOLDENROD = ((184, 134, 11), (255, 185, 15), (238, 173, 14), (205, 149, 12), (139, 101, 8))
+# Array of agent's colors
+
+
+
+class Color:
+    def __init__(self, index, start_color, stop_color, path_color, impact_color, search_color):
+        self.index = index
+        self.start_color = start_color
+        self.stop_color = stop_color
+        self.path_color = path_color
+        self.impact_color = impact_color
+        self.search_color = search_color
+
+    def chose_color(self):
+        return COLORS[self.index]
+
+#KHAKI = Color(0, (240, 230, 140), (255, 246, 143), (238, 230, 133), (205, 198, 115), (139, 134, 78))
+salmon = Color(0, (250,128,114), (255,140,105), (238,130,98), (205,112,84), (139,76,57))
+PALEGREEN = Color(1, (152, 251, 152), (154, 255, 154), (144, 238, 144), (124, 205, 124), (84, 139, 84))
+LIGHTPINK = Color(2, (255, 182, 193), (255, 174, 185), (238, 162, 173), (205, 140, 149), (139, 95, 101))
+ORCHID = Color(3, (218, 112, 214), (255, 131, 250), (238, 122, 233), (205, 105, 201), (139, 71, 137))
+LIGHTSKYBLUE = Color(4, (135, 206, 250), (176, 226, 255), (164, 211, 238), (141, 182, 205), (96, 123, 139))
+CORAL = Color(5, (255, 127, 80), (255, 114, 86), (238, 106, 80), (205, 91, 69), (139, 62, 47))
+DARKGOLDENROD = Color(6, (184, 134, 11), (255, 185, 15), (238, 173, 14), (205, 149, 12), (139, 101, 8))
+
+COLORS = [salmon, PALEGREEN, LIGHTPINK, ORCHID, LIGHTSKYBLUE, CORAL, DARKGOLDENROD]
+
+class Robot:
+    def __init__(self, start, end, curr, speed, color_index, priority, impact_rad, search_rad, path):
+        self.start = start
+        self.end = end
+        self.curr = curr
+        self.color_index = color_index
+        self.speed = speed
+        self.priority = priority
+        self.impact_rad = impact_rad
+        self.search_rad = search_rad
+        self.impact_rad_points = self.generate_circle(self.curr, self.impact_rad)
+        self.search_rad_points = self.generate_circle(self.curr, self.search_rad)
+        self.path = []
+
+    def fix_circle_points(self, input_list):
+        seen = set()
+        output_list = []
+
+        for item in input_list:
+            if item not in seen:
+                if(not(on_board(item[0]) and on_board(item[1]))):
+                    continue
+                output_list.append(item)
+                seen.add(item)
+
+        return output_list
+
+    def generate_circle(self, spot, radius=3, num_points=100):
+        points = []
+        theta = np.linspace(0, 2 * np.pi, num_points)
+        x = spot.row + radius * np.cos(theta)
+        y = spot.col + radius * np.sin(theta)
+        for x, y in zip(np.round(x).astype(int), np.round(y).astype(int)):
+            points.append((x, y))
+        return (self.fix_circle_points(points))
+
+    def __lt__(self, other):
+        return False
 
 class Spot:
 	def __init__(self, row, col, width, total_rows):
@@ -199,65 +280,81 @@ def get_clicked_pos(pos, rows, width):
 
 
 def main(win, width):
-	ROWS = 20
-	grid = make_grid(ROWS, width)
+    #ROWS = 20
+    #R = Robot((5, 5), (1, 1), (5, 5), 1, 1, 1, 3, 4)
+    MAX_ROBOTS = 7
+    robot_index = 0
+    grid = make_grid(ROWS, width)
+    Robot_List = []
+    robot_start_array = []
+    robot_end_array = []
+    mid_click = False
+    start_end = True
+    run = True
+    show_rad = False
+    while run:
+        draw(win, grid, ROWS, width)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
 
-	robot_start_array = []
-	robot_end_array = []
-	mid_click = False
-	start_end = True
+            if pygame.mouse.get_pressed()[0]:  # LEFT
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_pos(pos, ROWS, width)
+                spot = grid[row][col]
+                if not mid_click and robot_index < MAX_ROBOTS:
+                    if start_end:
+                        Robot_List.append(Robot(spot, spot, spot, 1, COLORS[robot_index], 1, 3, 4, None))
+                        Spot.make_start(Robot_List[robot_index].start, Robot_List[robot_index].color_index)
+                        start_end = False
 
-	run = True
-	while run:
-		draw(win, grid, ROWS, width)
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				run = False
+                    else:
+                        Robot_List[robot_index].end = spot
+                        Spot.make_end(Robot_List[robot_index].end, Robot_List[robot_index].color_index)
+                        start_end = True
+                        robot_index += 1
 
-			if pygame.mouse.get_pressed()[0]: # LEFT
-				pos = pygame.mouse.get_pos()
-				row, col = get_clicked_pos(pos, ROWS, width)
-				spot = grid[row][col]
-				if not mid_click:
-					if start_end:
-						robot_start_array.append(spot)
-						Spot.make_start(robot_start_array[-1])
-						start_end = False
-					else:
-						robot_end_array.append(spot)
-						Spot.make_end(robot_end_array[-1])
-						start_end = True
+                else:
+                    if spot.check_point(Robot_List):
+                        spot.make_barrier()
+            elif pygame.mouse.get_pressed()[1]:  # Mid
+                mid_click = True
+                print("number of robots: " + str(robot_index))
+            elif pygame.mouse.get_pressed()[2]:  # RIGHT
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_pos(pos, ROWS, width)
+                spot = grid[row][col]
+                spot.reset()
+                if spot == start:
+                    start = None
+                elif spot == end:
+                    end = None
 
-				elif spot.check_point(robot_start_array, robot_end_array):
-					spot.make_barrier()
-			elif pygame.mouse.get_pressed()[1]:  # Mid
-				mid_click = True
-			elif pygame.mouse.get_pressed()[2]: # RIGHT
-				pos = pygame.mouse.get_pos()
-				row, col = get_clicked_pos(pos, ROWS, width)
-				spot = grid[row][col]
-				spot.reset()
-				if spot == start:
-					start = None
-				elif spot == end:
-					end = None
+            if event.type == pygame.KEYDOWN:
+                for i in range(robot_index):
+                    if event.key == pygame.K_SPACE: #and robot_start_array[i] and robot_end_array[i]:
+                        for row in grid:
+                            for spot in row:
+                                spot.update_neighbors(grid)
 
-			if event.type == pygame.KEYDOWN:
-				for i in range(len(robot_start_array)):
-					if event.key == pygame.K_SPACE and robot_start_array[i] and robot_end_array[i]:
-						for row in grid:
-							for spot in row:
-								spot.update_neighbors(grid)
+                        algorithm(lambda: draw(win, grid, ROWS, width), grid, Robot_List[i])
 
-						algorithm(lambda: draw(win, grid, ROWS, width), grid, robot_start_array[i], robot_end_array[i])
+                    if event.key == pygame.K_c:
+                        mid_click = False
+                        start_end = True
+                        robot_index = 0
+                        grid = make_grid(ROWS, width)
 
-					if event.key == pygame.K_c:
-						robot_start_array = []
-						robot_end_array = []
-						mid_click = False
-						start_end = True
-						grid = make_grid(ROWS, width)
+                    if event.key == pygame.K_r:
+                        grid = make_grid(ROWS, width)
+                        #row, col, width, total_rows, robot
+                        if not show_rad:
+                            for robot in Robot_List:
+                                for spot in robot.impact_rad_points:
+                                    spot = Spot(spot[0],spot[1], width, ROWS, robot)
+                                    spot.draw_impact_circle(robot.color_index)
 
-	pygame.quit()
+
+    pygame.quit()
 
 main(WIN, WIDTH)
